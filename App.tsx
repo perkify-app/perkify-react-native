a52eda02bee2c2980fec241dc088f4eea3394fbd
mkawi <mkawi@github.com>
2024-01-02 14:27:01 +0000

    Add GluestackProvider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { GluestackUIProvider} from "@gluestack-ui/themed";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+
+import { Button, ButtonSpinner, ButtonText } from "@gluestack-ui/themed"
+
+let count: number = 0
 
 export default function App() {
 	return (
 		<GluestackUIProvider>
 			<View style={styles.container}>
-				<Text>Open up App.tsx to start working on your app!</Text>
+				<Button isDisabled={true} bg="$darkBlue600" p="$3">
+					<ButtonSpinner mr="$1" />
+					<ButtonText fontWeight="$medium" fontSize="$sm">
+						Please wait...
+					</ButtonText>
+				</Button><Button />
+				<Text>Open up App.tsx to start working on your app! fsdfsdfsgfdfdgfdggfddfsdf</Text>
 				<StatusBar style="auto" />
 			</View>
 		</GluestackUIProvider>
