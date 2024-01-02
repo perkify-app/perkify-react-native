ae2731071f156cea712b968d77a40f465a0b17d9
mkawi <mkawi@github.com>
2024-01-02 20:29:32 +0000

    reset repo

--- components/__tests__/example.test.ts
+++
@@ -1,9 +0,0 @@
-describe("Sample test suite", () => {
-	test("Initial passing test", () => {
-		expect(2).toEqual(2);
-	});
-
-	test("Initial failing test", () => {
-		expect(2).toEqual(3);
-	});
-});
