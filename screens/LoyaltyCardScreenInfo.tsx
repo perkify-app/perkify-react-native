6fe105f7f2f6eaa1f3852e8d7123c9d7fad6a980
dannygorgon <thirdoptionisthename@googlemail.com>
2024-01-03 14:36:29 +0000

    add merchant and merchantName variables to find and render the correct merchant_name, where the loyalty_program_id === merchant_id

--- screens/LoyaltyCardScreenInfo.tsx
+++ screens/LoyaltyCardScreenInfo.tsx
@@ -1,18 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import { LoyaltyCards } from "../constants/mock-data/LoyaltyCards";
 import StampCard from "./components/StampCard";
+import { Merchants } from "../constants/mock-data/Merchants";
+
+console.log(Merchants);
 
 export default function LoyaltyCardScreenInfo() {
-  const card = LoyaltyCards[2]; // mocked data for now
-  return (
-    <View style={styles.exampleContainer}>
-      {/* <Text style={styles.exampleText}>Loyalty Program ID: {card.loyalty_program_id}</Text> */}
-      <Text style={styles.exampleText}>  You've got {card.points} stamp{card.points === 1 ? '' : 's'}!</Text>
-      {/* <Text style={styles.exampleText}>Created At: {card.created_at}</Text> */}
-      <StampCard stamps={card.points} />
-    </View>
-  );
-}
+    const card = LoyaltyCards[1]; // mocked data for now
+    const merchant = Merchants.find(m => m.merchant_id === card.loyalty_program_id);
+    const merchantName = merchant ? merchant.company_name : 'Unknown Merchant';
+  
+    return (
+      <View style={styles.exampleContainer}>
+        <Text style={styles.exampleText}>Merchant: {merchantName}</Text>
+        <Text style={styles.exampleText}>You've got {card.points} stamp{card.points === 1 ? '' : 's'}!</Text>
+        <StampCard stamps={card.points} />
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   exampleContainer: {
