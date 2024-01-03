3482913548b1bab3f6af546bf9980fa73aea8380
Kevin Zehner <kevzehner@gmail.com>
2024-01-03 10:10:08 +0000

    adds Merchants screen

--- app/(tabs)/merchants/index.tsx
+++ app/(tabs)/merchants/index.tsx
@@ -1,30 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 import ExampleScreenInfo from "../../../screens/ExampleScreenInfo";
 
 export default function MerchantListScreen() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Merchants</Text>
-			<View style={styles.separator} />
-			<ExampleScreenInfo />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <QRCode value="https://www.google.com" />
+      <Text style={styles.title}>View Merchants with promotions</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Merchant 1</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Merchant 2</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Merchant 3</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Merchant 4</Text>
+      </TouchableOpacity>
+      <View style={styles.separator} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: "80%",
-	},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    paddingLeft: 100,
+    paddingRight: 100,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#000000",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
 });
