2f7daaf14365fe200155409956a1406af3889902
Kevin Zehner <kevzehner@gmail.com>
2024-01-03 11:47:06 +0000

    adds user ID field

--- app/(tabs)/merchants/index.tsx
+++ app/(tabs)/merchants/index.tsx
@@ -8,11 +8,16 @@ import {
 import QRCode from "react-native-qrcode-svg";
 
 import { Merchants } from "../../../constants/mock-data/Merchants";
+import { Users } from "../../../constants/mock-data/Users";
 
 export default function MerchantListScreen() {
   return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
       <QRCode size={200} value="https://www.google.com" />
+      <View style={styles.separator}>
+        <Text style={styles.sub_heading}>User ID</Text>
+        <Text style={styles.account_userId}>{Users[0].id}</Text>
+      </View>
       <Text style={styles.title}>View Merchants with promotions</Text>
       {Merchants.map((merchant) => (
         <TouchableOpacity style={styles.button} key={merchant.merchant_id}>
@@ -33,7 +38,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    marginTop: 20,
+    marginTop: 60,
     marginBottom: 20,
   },
   button: {
@@ -52,5 +57,21 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+    alignItems: "center",
+  },
+  sub_heading: {
+    fontWeight: "700",
+    textAlign: "center",
+    marginBottom: 5,
+  },
+  account_userId: {
+    backgroundColor: "white",
+    borderRadius: 7,
+    fontSize: 24,
+    width: 200,
+    textAlign: "center",
+    paddingVertical: 10,
+    marginTop: 5,
+    marginBottom: 30,
   },
 });
