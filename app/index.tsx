bd0f1b3de214e553367539dccd4a87e05f4954c4
mkawi <mkawi@github.com>
2024-01-03 02:09:03 +0000

    add initial basic screens: rootLayout, loginScreen, notFoundScreen, modalScreen

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import LoginScreenInfo from "../components/LoginScreenInfo";
+
+export default function LoginScreen() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Example Login Screen</Text>
+			<LoginScreenInfo />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+});
