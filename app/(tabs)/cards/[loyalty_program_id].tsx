9f763568c8472258bf74f6f4a2075c28e7475a8b
Kevin Zehner <kevzehner@gmail.com>
2024-01-04 16:30:47 +0000

    adds back button

--- app/(tabs)/cards/[loyalty_program_id].tsx
+++ app/(tabs)/cards/[loyalty_program_id].tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import { LoyaltyCards } from "../../../constants/mock-data/LoyaltyCards";
 import StampCard from "../../../screens/components/StampCard";
 import { Merchants } from "../../../constants/mock-data/Merchants";
 import QRCode from "react-native-qrcode-svg";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, Link } from "expo-router";
 
 console.log(Merchants);
 
@@ -14,6 +14,7 @@ export default function LoyaltyCardScreenInfo() {
     return card.loyalty_program_id === Number(id.loyalty_program_id);
   });
   const card = filteredCard[0];
+  console.log(card);
 
   const merchant = Merchants.find(
     (m) => m.merchant_id === card.loyalty_program_id
@@ -21,21 +22,28 @@ export default function LoyaltyCardScreenInfo() {
   const merchantName = merchant ? merchant.company_name : "Unknown Merchant";
 
   return (
-    <View style={styles.exampleContainer}>
-      <QRCode size={200} value="https://www.google.com" />
-      <View style={styles.separator} />
-      <Text style={styles.exampleText}>Merchant: {merchantName}</Text>
-      <Text style={styles.exampleText}>
-        You've got {card.points} stamp{card.points === 1 ? "" : "s"}!
-      </Text>
-      <StampCard stamps={card.points} />
+    <View>
+      <Pressable style={styles.back}>
+        <Link href="/cards">Back</Link>
+      </Pressable>
+      <View style={styles.exampleContainer}>
+        <QRCode size={200} value="https://www.google.com" />
+        <View style={styles.separator} />
+        <Text style={styles.exampleText}>Merchant: {merchantName}</Text>
+        <Text style={styles.exampleText}>
+          You've got {card.points} stamp{card.points === 1 ? "" : "s"}!
+        </Text>
+        <StampCard stamps={card.points} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   exampleContainer: {
+    marginTop: 50,
     alignItems: "center",
+    justifyContent: "center",
     marginHorizontal: 50,
   },
   exampleText: {
@@ -47,5 +55,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  back: {
+    marginTop: 50,
+    marginLeft: 20,
   },
 });
