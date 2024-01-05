7f800a9c0ad6fa49db9db98ee13cda76d4396390
mkawi <mkawi@github.com>
2024-01-05 09:34:26 +0000

    add InputField components to Login Screen

--- screens/LoginScreenInfo.tsx
+++ screens/LoginScreenInfo.tsx
@@ -1,15 +1,28 @@
-import { router } from "expo-router";
+import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+
+import { InputField } from "./components/InputField";
+
+// Use Supabase Auth React Native components here. All other screens and
+// main-nav should not be accessible unless user isAuthenticated. If user
+// is already logged in, should redirect to merchants/overview screen
 
 export default function LoginScreenInfo() {
+	const [email, setEmail] = useState("johndoe@gmail.com");
+	const [pass, setPass] = useState("password123");
+
 	return (
-		<View>
-			<View style={styles.loginContainer}>
-				<Text style={styles.loginText}>
-					Use Supabase Auth React Native components here. All other screens and
-					main-nav should not be accessible unless user isAuthenticated. If user
-					is already logged in, should redirect to merchants/overview screen
-				</Text>
+		<View style={styles.container}>
+			<Text style={styles.title}>Login</Text>
+			<InputField header="Email" state={email} onChange={setEmail} />
+			<InputField
+				header="Password"
+				state={pass}
+				onChange={setPass}
+				hide={true}
+			/>
+			<View style={styles.btnContainer}>
 				<TouchableOpacity
 					style={styles.btn}
 					onPress={() => router.replace("/(tabs)/merchants")}
@@ -28,9 +41,17 @@ export default function LoginScreenInfo() {
 }
 
 const styles = StyleSheet.create({
-	loginContainer: {
+	container: {
+		backgroundColor: "white",
 		alignItems: "center",
-		marginHorizontal: 50,
+		width: "75%",
+		maxWidth: 600,
+		padding: 30,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 24,
 	},
 	loginText: {
 		padding: 20,
@@ -38,15 +59,20 @@ const styles = StyleSheet.create({
 		lineHeight: 30,
 		textAlign: "center",
 	},
+	btnContainer: {
+		marginTop: 10,
+	},
 	btn: {
 		backgroundColor: "black",
-		paddingVertical: 17,
+		paddingVertical: 20,
 		paddingHorizontal: 35,
 		marginVertical: 10,
 		borderRadius: 10,
+		minWidth: 200,
 	},
 	btnText: {
 		color: "white",
+		textAlign: "center",
 	},
 	registerBtn: {
 		backgroundColor: "gray",
